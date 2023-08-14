5ff7cd1697d062d99481c04ba9b5b1d6a9a00beb
Surdiana <dwisurdiana88@gmail.com>
2023-08-14 19:02:17 +0700

    connection pooling

--- mongo.go
+++ mongo.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"context"
-	"fmt"
 	"os"
 	"time"
 
@@ -10,17 +9,43 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func MongoConnectDB(db string, coll string) (*mongo.Collection, error) {
-	// Use the SetServerAPIOptions() method to set the Stable API version to 1
-	fmt.Println(os.Getenv("DB_SERVER") + "://" + os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST"))
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+var (
+	client          *mongo.Client
+	collectionCache = map[string]*mongo.Collection{}
+)
 
+func MongoConnectDB(db string, coll string) (*mongo.Collection, *mongo.Client, error) {
+	if client != nil {
+		return GetCollection(db, coll), client, nil
+	}
+	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	opts := options.Client().ApplyURI(os.Getenv("DB_SERVER") + "://" + os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + "/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
+
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(ctx, opts)
 
-	return client.Database(db).Collection(coll), err
+	// Mengecek apakah koneksi sukses
+	if client.Ping(ctx, nil) != nil {
+		client.Disconnect(context.Background())
+		client = nil
+		return nil, client, err
+	}
+
+	return GetCollection(db, coll), client, err
+}
+
+func GetCollection(db string, collName string) *mongo.Collection {
+
+	// Mengecek apakah koleksi sudah ada di cache, jika tidak, membuatnya
+	if coll, ok := collectionCache[collName]; ok {
+		return coll
+	}
+
+	coll := client.Database(db).Collection(collName)
+	collectionCache[collName] = coll
+	return coll
 }
