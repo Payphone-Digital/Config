3ae5ac09d741cf96a3d8337de504655e61e441d6
Surdiana <dwisurdiana88@gmail.com>
2023-08-15 10:50:03 +0700

    connection pooling & management connection

--- mongo.go
+++ mongo.go
@@ -3,6 +3,9 @@ package config
 import (
 	"context"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,35 +15,63 @@ import (
 var (
 	client          *mongo.Client
 	collectionCache = map[string]*mongo.Collection{}
+	cacheMutex      sync.Mutex
+	clientMutex     sync.Mutex
 )
 
 func MongoConnectDB(db string, coll string) (*mongo.Collection, *mongo.Client, error) {
-	if client != nil {
-		return GetCollection(db, coll), client, nil
+	clientMutex.Lock()
+	defer clientMutex.Unlock()
+
+	// Mencoba mengambil koleksi dari cache
+	if coll, ok := collectionCache[coll]; ok {
+		return coll, client, nil
 	}
-	// Use the SetServerAPIOptions() method to set the Stable API version to 1
+
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(os.Getenv("DB_SERVER") + "://" + os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + "/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
+	opts := options.Client().
+		ApplyURI(os.Getenv("DB_SERVER") + "://" + os.Getenv("DB_USERNAME") + ":" + os.Getenv("DB_PASSWORD") + "@" + os.Getenv("DB_HOST") + "/?retryWrites=true&w=majority").
+		SetServerAPIOptions(serverAPI).SetMinPoolSize(5).
+		SetMaxPoolSize(100) // Atur ukuran maksimum dari connection pool
 
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(ctx, opts)
+	if client == nil {
+		var err error
+		client, err = mongo.Connect(ctx, opts)
+		if err != nil {
+			return nil, client, err
+		}
 
-	// Mengecek apakah koneksi sukses
-	if client.Ping(ctx, nil) != nil {
-		client.Disconnect(context.Background())
-		client = nil
-		return nil, client, err
+		// Menambahkan goroutine untuk menangani sinyal shutdown
+		go func() {
+			sigint := make(chan os.Signal, 1)
+			signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+
+			<-sigint
+			// Menerima sinyal shutdown, menutup koneksi dengan aman
+			if client != nil {
+				client.Disconnect(context.Background())
+			}
+			os.Exit(0)
+		}()
 	}
 
-	return GetCollection(db, coll), client, err
+	err := client.Ping(ctx, nil)
+	if err != nil {
+		client.Disconnect(context.Background())
+		client = nil
+		return nil, nil, err
+	}
+
+	return GetCollection(db, coll), client, nil
 }
 
 func GetCollection(db string, collName string) *mongo.Collection {
+	cacheMutex.Lock()
+	defer cacheMutex.Unlock()
 
-	// Mengecek apakah koleksi sudah ada di cache, jika tidak, membuatnya
 	if coll, ok := collectionCache[collName]; ok {
 		return coll
 	}
